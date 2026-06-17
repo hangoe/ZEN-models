0fb490f112f9a68556160a05f1dc3d7fe3491952
Hanne Goericke <hgoericke@ethz.ch>
2026-06-17 09:48:33 +0200

    adapting path to run Crystal_Ball_HG_v2_0

--- run_model.py
+++ run_model.py
@@ -1,11 +1,19 @@
 from pathlib import Path
+from zen_garden import run, Results
 
-from zen_garden import run
+my_dataset = str("Crystal_Ball_HG_v2_0")
 
-DATA_DIR = Path(__file__).parent / "data"
+# DATA_DIR = Path(__file__).parent / "data"
+
+DATA_DIR_CONFIG = Path(__file__).parent / "data"
+DATA_DIR = Path(__file__).parent.parent / "ZEN-creator" / "outputs"
 
 if __name__ == "__main__":
     run(
-        config=str(DATA_DIR / "config.json"),
-        dataset=str(DATA_DIR / "Crystal_Ball"),
+        config=str(DATA_DIR_CONFIG / "config.json"),
+        dataset=str(DATA_DIR / my_dataset),
     )
+
+#r = Results(path=DATA_DIR_CONFIG / "outputs" / my_dataset)
+
+print(Results)
